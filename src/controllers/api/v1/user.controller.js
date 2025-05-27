5d7b9977fb18a7d328166731436957784577abff
TibboVL <r0879836@student.thomasmore.be>
2025-05-27 10:36:39 +0200

    feat: move uit api stuff to admin + add migrate up/down endpoint in admin for remove db management

--- src/controllers/api/v1/user.controller.js
+++ src/controllers/api/v1/user.controller.js
@@ -3,44 +3,6 @@ import { getUitEventDecodedList } from "../../../services/UitVlaanderenService.j
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 import { sendError, sendSuccess } from "../../../utils/responses.js";
 
-// temporary
-export const getUsers = asyncHandler(async (req, res) => {
-  const works = false;
-
-  console.log(req.body);
-  console.log(req.body.username);
-
-  if (works) {
-    sendSuccess(res, {
-      statusCode: 200,
-      message: "Users loaded succesfully",
-      data: [{ id: 1, name: "Alice" }],
-    });
-  } else {
-    sendError(res, {
-      statusCode: 400,
-      message: "Validation failed",
-      errors: [
-        "Password must be more than 8 characters",
-        "Password must include at least one special character",
-      ],
-    });
-  }
-});
-
-export const getUitEvents = asyncHandler(async (req, res) => {
-  console.log(req.query);
-  const events = await getUitEventDecodedList(req.query);
-
-  //console.log(events);
-
-  sendSuccess(res, {
-    statusCode: 200,
-    message: "UitVlaanderen Events Retrieved",
-    data: events,
-  });
-});
-
 export async function getOrCreateUser(auth0User) {
   let user = await db("user").where({ auth0Id: auth0User.payload.sub }).first();
 
--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import userRoutes from "./user.routes.js";
 import activityRoutes from "./activity.routes.js";
 import moodRoutes from "./mood.routes.js";
 import checkinRoutes from "./checkin.routes.js";
 import userActivityListRoutes from "./userActivityList.routes.js";
+import adminRoutes from "./admin.routes.js";
 
 const router = Router();
 
 // register all routes here
-router.use("/users", userRoutes);
+router.use("/admin", adminRoutes);
 router.use("/activities", activityRoutes);
 router.use("/moods", moodRoutes);
 router.use("/checkin", checkinRoutes);
